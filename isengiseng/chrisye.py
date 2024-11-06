eff5016cfe2cafd8d0dc81d4f87103de7b318f58
Eizen_enzu <lebotax123@gmail.com>
2024-11-06 11:22:49 +0700

    Add files via upload

---
+++ isengiseng/chrisye.py
@@ -0,0 +1,64 @@
+import time,sys
+
+print("============================")
+print("Seperti yang kau minta",)
+print("cipt: Chrisye",)
+print("============================")
+print("")
+time.sleep(4)
+
+lirik1 = "Maafkan aku tak bisa memahami maksud amarahmu,\nMembaca dan mengerti isi hatimu.\nAmpuni aku yg telah memasuki kehidupan kalian,\nMencoba mencari celah dalam hatimu\n"
+for char in lirik1:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.25)
+print ("")
+time.sleep(5)
+
+lirik2 = "Aku tau ku takkan bisa menjadi s'perti yg engkau minta,\nNamun selama nafas berhembus aku kan mencoba\nmenjadi s’perti yg kau minta\n"
+for char in lirik2:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.20)
+print("")
+time.sleep(7)
+
+lirik3 = "Ampuni aku yg telah memasuki kehidupan kalian\nMencoba mencari celah dalam hatimu\n"
+for char in lirik3:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.20)
+print("")
+time.sleep(2)
+
+lirik4 = "Aku tau ku takkan bisa menjadi s'perti yg engkau minta\nNamun selama nafas berhembus aku kan mencoba\nAku tau dia yg bisa menjadi s'perti yg engkau minta\nNamun selama aku bernyawa aku kan mencoba menjadi s’perti yg kau minta\n"
+for char in lirik4:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.25)
+print("")
+time.sleep(9)
+
+lirik5 = "Aku tau ku takkan bisa menjadi s'perti yg engkau minta\nNamun selama nafas berhembus aku kan mencoba\nAku tau dia yg bisa menjadi s'perti yg engkau minta\nNamun selama aku bernyawa aku kan mencoba menjadi s'perti yg kau minta\nSeperti yg kau minta.\n"
+for char in lirik5:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.25)
+print("")
+time.sleep(6)
+
+lirik6 = "aku kan mencoba "
+for char in lirik6:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.25)
+time.sleep(3)
+
+lirik7 = "menjadi seperti yang kau minta"
+for char in lirik7:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.25)
+print("")
+print("==============================")
+time.sleep(7)
